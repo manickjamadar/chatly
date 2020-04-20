55f6d9e4b62cacfa8d5bb90154076b2d34be1885
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-20 13:20:58 +0530

    message date formatter created

---
+++ lib/helpers/message_date_formatter.dart
@@ -0,0 +1,18 @@
+import 'package:intl/intl.dart';
+
+String messageDateFormatter(DateTime dateTime) {
+  final dayDifference = DateTime.now().difference(dateTime).inDays;
+  if (dayDifference == 0) {
+    return messsageTimeFormatter(dateTime);
+  } else if (dayDifference == 1) {
+    return 'yesterday';
+  } else if (dayDifference == -1) {
+    return 'tomorrow';
+  } else {
+    return DateFormat("d MMM").format(dateTime);
+  }
+}
+
+String messsageTimeFormatter(DateTime dateTime) {
+  return DateFormat('h:m').format(dateTime);
+}
