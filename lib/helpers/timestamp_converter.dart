25ee0c5c35e3491485d99df5d29d56e4b6ee83dc
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-20 12:34:44 +0530

    update logic of timestamp converter

--- lib/helpers/timestamp_converter.dart
+++ lib/helpers/timestamp_converter.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 DateTime timestampToDateTime(Timestamp stamp) {
-  return DateTime.fromMicrosecondsSinceEpoch(
-      stamp.microsecondsSinceEpoch * 1000);
+  if (stamp == null) return null;
+  return stamp.toDate();
 }
