5f86790318fa3680aea3291edd66d1f6df304379
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-23 11:57:20 +0530

    all profile provider name change from non active profile to all profile

--- lib/providers/all_profile_provider.dart
+++ lib/providers/all_profile_provider.dart
@@ -6,11 +6,11 @@ import 'package:chatly/service/database_service.dart';
 class AllProfileProvider extends ViewStateProvider {
   DatabaseService _databaseService;
   List<Profile> _activeProfiles;
-  List<Profile> _nonActiveProfiles;
+  List<Profile> _allProfiles;
 
   //getters
   List<Profile> get activeProfiles => _activeProfiles;
-  List<Profile> get nonActiveProfiles => _nonActiveProfiles;
+  List<Profile> get allProfiles => _allProfiles;
 
   //create
   AllProfileProvider(DatabaseService databaseService)
@@ -22,7 +22,7 @@ class AllProfileProvider extends ViewStateProvider {
     startInitialLoader();
     if (_databaseService == null) return;
     try {
-      _nonActiveProfiles = await _databaseService.getAllProfile();
+      _allProfiles = await _databaseService.getAllProfile();
       stopExecuting();
     } on Failure catch (failure) {
       print(failure);
