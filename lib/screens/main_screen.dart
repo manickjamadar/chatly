dbbd769d317e659ea59fabe5bd2a76bc4d83c8ca
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-23 12:59:43 +0530

    create active chat view for active profile

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:chatly/models/profile.dart';
 import 'package:chatly/providers/profile_provider.dart';
 import 'package:chatly/providers/view_state_provider.dart';
 import 'package:chatly/screens/select_profile_screen.dart';
+import 'package:chatly/views/active_chat_view.dart';
 import 'package:chatly/widgets/profile_name.dart';
 import 'package:chatly/views/profile_option_popup_button.dart';
 import 'package:chatly/widgets/profile_viewer.dart';
@@ -83,9 +84,7 @@ class MainScreen extends StatelessWidget {
               ),
               body: TabBarView(
                 children: <Widget>[
-                  Center(
-                    child: Text("No User Available"),
-                  ),
+                  ActiveChatView(),
                   Center(
                     child: Text("Status Coming Soon"),
                   ),
