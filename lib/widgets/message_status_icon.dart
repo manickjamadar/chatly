2ee21aa15807a0b20c4bb951e615f2ec1825ce97
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-24 12:04:14 +0530

    add icon size support to message status icon

--- lib/widgets/message_status_icon.dart
+++ lib/widgets/message_status_icon.dart
@@ -4,22 +4,24 @@ import "package:flutter/material.dart";
 class MessageStatusIcon extends StatelessWidget {
   final MessageStatus status;
   final Color color;
-  MessageStatusIcon(this.status, {this.color = Colors.grey});
+  final double size;
+  MessageStatusIcon(this.status, {this.color = Colors.grey, this.size = 15});
   @override
   Widget build(BuildContext context) {
     switch (status) {
       case MessageStatus.sent:
-        return Icon(Icons.check, color: color);
+        return Icon(Icons.check, color: color, size: size);
         break;
       case MessageStatus.delivered:
-        return Icon(Icons.done_all, color: color);
+        return Icon(Icons.done_all, color: color, size: size);
       case MessageStatus.seen:
         return Icon(
           Icons.done_all,
+          size: size,
           color: Colors.blue,
         );
       default:
-        return Icon(Icons.check, color: color);
+        return Icon(Icons.check, color: color, size: size);
     }
   }
 }
