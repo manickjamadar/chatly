37737559edecef8a00c7ce089df8e8b3afb89706
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-30 17:35:23 +0530

    mock push message handler created in main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatly/models/message.dart';
 import 'package:chatly/models/profile.dart';
 import 'package:chatly/providers/profile_provider.dart';
 import 'package:chatly/providers/view_state_provider.dart';
@@ -8,11 +9,47 @@ import 'package:chatly/widgets/profile_avatart.dart';
 import 'package:chatly/widgets/profile_name.dart';
 import 'package:chatly/views/profile_option_popup_button.dart';
 import 'package:chatly/widgets/profile_viewer.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   static const String mainScreen = "/main-screen";
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(onMessage: (Map<String, dynamic> fcm) async {
+      // final Map<String, dynamic> data = fcm['data'];
+      // final bool isMessageAvailable = data['isMessageAvailable'];
+      // if (isMessageAvailable == null || !isMessageAvailable) return;
+      // final Message message = Message.fromMap(data['data']);
+      //do something with message;
+      print("\n\n\n\n\n\n\n\n\n\n\n\n");
+      print("On Message:");
+      // print("Message Instance: ${message.toMap()}");
+    }, onResume: (Map<String, dynamic> data) async {
+      // final Message message = Message.fromMap(data['data']);
+      //do something with resume
+      print("\n\n\n\n\n\n\n\n\n\n\n\n");
+      print("On Resume:");
+      // print("Message Instance: ${message.toMap()}");
+    }, onLaunch: (Map<String, dynamic> data) async {
+      // final Message message = Message.fromMap(data['data']);
+      //do something with launch
+      print("\n\n\n\n\n\n\n\n\n\n\n\n");
+      print("On Launch:");
+      print(data);
+      // print("Message Instance: ${message.toMap()}");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final ProfileProvider profileProvider =
